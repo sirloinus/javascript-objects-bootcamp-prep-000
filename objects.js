98fefeb0c32d9f1cbeacdbab0e6b7c4d21663428
Amalie S <amaliesmidth@gmail.com>
2018-06-03 19:30:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+
+function removeFromPlaylist (playlist, artistName) {
+  playlist.delete = artistName
+}
