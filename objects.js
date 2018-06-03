927389617ce5bb26dfad6d14b25f0c468198102d
Amalie S <amaliesmidth@gmail.com>
2018-06-03 19:23:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,18 @@ function updatePlaylist (playlist, artistName, songTitle) {
   playlist['Nils Frahm'] = 'Sunson'
   return playlist
 }
+
+
+ describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
